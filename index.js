f548ddcca347664d23a240ac4a6c518cae9e099c
ramyar omed ali <ro21090@auis.edu.krd>
2022-11-24 00:45:09 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const e = document.querySelector('main')
+e.remove();
+
+const newHeader = document.createElement('H1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "Ramyar is the champion";
